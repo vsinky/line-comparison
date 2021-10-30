14937dde3c50da92dc5490371d24545190410948
vsinky <sinkykumariverma@gmail.com>
2021-10-31 00:56:33 +0530

    compare line and check is greater or less or equal

--- src/com/bridgelabz/demo/line.java
+++ src/com/bridgelabz/demo/line.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.demo;
 
+import java.util.Scanner;
+
 public class line {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation");
@@ -8,19 +10,23 @@ public class line {
 		int y1 = 8;
 		int y2 = 89;
 		int p1 = 14;
-		int p2 = 16;
+		int p2 = 15;
 		int q1 = 4;
 		int q2 = 85;
 
 		double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 		double lengthOfLine2 = Math.sqrt((p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1));
-		System.out.println("Length of Line1 : " + lengthOfLine1);
-		System.out.println("Length of Line2 : " + lengthOfLine2);
 
-		if (lengthOfLine1 == lengthOfLine2) {
-			System.out.println("Both lines are equal.");
+		System.out.println("line 1st length : " + lengthOfLine1);
+		System.out.println("line 2nd length : " + lengthOfLine2);
+
+		if (lengthOfLine1 > lengthOfLine2) {
+			System.out.println("Length of Line1 is greater than Length of Line2");
+		} else if (lengthOfLine1 > lengthOfLine2) {
+			System.out.println("Length of Line1 is lesser than Length of Line2");
 		} else {
-			System.out.println("Both lines are not equal.");
+			System.out.println("Both lines are Equal");
 		}
 	}
+
 }
